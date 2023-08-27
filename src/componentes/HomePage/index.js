db0a946ba888b9290d915b455977be47461426ec
Robson Luan <rluan95@hotmail.com>
2023-08-27 19:06:23 -0300

    commit initial

---
+++ src/componentes/HomePage/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "../Card/Card";
+import "./style.css";
+
+export default function HomePage(){
+    return(
+        <div className="Home">
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
+    )
+}
